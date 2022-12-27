595decf2744247cf00f044cabdb11e0635a807f7
lioarce01 <lioarce1@gmail.com>
2022-12-27 19:04:51 -0300

    leetcode day 7

--- Process Logs.js
+++ Process Logs.js
@@ -1,12 +1,16 @@
 function processLogs(logs, maxSpan) {
 	const result = new Map()
-
+  
 	for (const log of logs) {
+		//detructuring of array of logs
 		const [id, time, type] = log.split(" ")
+		//if the object result does not have id, then set an id, with the signIn and signOut properties empty
 		if (!result.has(id)) {
 			result.set(id, { id, signIn: "", signOut: "" })
 		}
+		//obtaining the id of every data in the object map
 		const obj = result.get(id)
+		//check if the type is sign in or sign out and set the equivalent time.
 		if (type === "sign-in") {
 			obj.signIn = time
 		} else {
@@ -15,10 +19,13 @@ function processLogs(logs, maxSpan) {
 	}
 
 	const timeDeltaIDs = []
+	//for loop over result map, and check if are signIn and signOut
 	for (const obj of result.values()) {
 		if (obj.signIn && obj.signOut) {
+			//update timeDelta with the substraction of signOut - signIn
 			obj.timeDelta = obj.signOut - obj.signIn
 			if (obj.timeDelta <= maxSpan) {
+				//push the timeDelta IDs that meets the condition into timeDeltaIDs array
 				timeDeltaIDs.push(obj.id)
 			}
 		}
