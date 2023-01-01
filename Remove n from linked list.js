7b3e223b68e3460ce0f962cd09db1559d09d6c22
lioarce01 <lioarce1@gmail.com>
2022-12-31 21:03:30 -0300

    leetcode day 12

--- Remove n from linked list.js
+++ Remove n from linked list.js
@@ -3,26 +3,29 @@ function ListNode(val, next) {
 	this.next = next === undefined ? null : next
 
 	var removeNthFromEnd = function (head, n) {
-		let slow = head
-		let fast = head
+		let slow = head //slow pointer to loop over elements
+		let fast = head //fast pointer to loop over elements + 1
 		let i = 0
 
 		while (i < n) {
-			fast = fast.next
+			//while i < n (removed element)
+			fast = fast.next //fast pointer go to next element
 			i++
 		}
 
 		if (!fast) {
+			//if there is no fast pointer just return next element
 			return head.next
 		}
 
 		while (fast.next) {
-			slow = slow.next
-			fast = fast.next
+			slow = slow.next //while we have a fast pointer, slow pointer goes to next element
+			fast = fast.next //fast pointer goes to the next element of slow
 		}
 
-		slow.next = slow.next.next
-		return head
+		slow.next = slow.next.next //skip the nth element from the list
+
+		return head //return the list without the skipped element
 	}
 	console.log(removeNthFromEnd([1, 2, 3, 4, 5], 2))
 }
